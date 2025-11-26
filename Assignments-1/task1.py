66b6460578a5cdce629b1fc640bcb162c6eb7ed0
VASU <bhutvasu07092004@gmail.com>
2025-11-26 14:36:44 +0530

    Added Assignment 1 tasks

---
+++ Assignments-1/task1.py
@@ -0,0 +1,15 @@
+n1 = int(input("Enter First Number : "))
+n2 = int(input("Enter First Number : "))
+
+print(f"Addition : {n1+n2}")
+print(f"Substraction : {n1-n2}")
+print(f"Multiplication : {n1*n2}")
+print(f"division : {n1/n2}")
+
+# Enter First Number : 5
+# Enter First Number : 10
+
+# Addition : 15
+# Substraction : -5
+# Multiplication : 50
+# division : 0.5
---
+++ Assignments-1/task2.py
@@ -0,0 +1,8 @@
+n1=input("Enter First Name : ")
+n2=input("Enter Last Name : ")
+
+print("Hello, " + n1 + " " + n2 + "! Welcome to the Python Program")
+
+# Enter First Name : Vasu
+# Enter Last Name : Bhut
+# Hello, Vasu Bhut! Welcome to the Python Program
